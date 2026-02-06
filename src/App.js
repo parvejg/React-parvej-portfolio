7d1459850621b0463d4d85c15288268dd8f41d5e
Md Parvez <parvezpg321@gmail.com>
2026-02-06 10:21:37 -0800

    up

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Home />} />
+        
         <Route path="/about" element={<About/>} />
         <Route path="/projects" element={<Projects/>} />
         <Route path="/contact" element={<Contact />} />
